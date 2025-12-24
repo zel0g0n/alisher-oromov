819856856d35f5c1a2508639664ee3627b3abdf8
zel0g0n <alisheroromov473@gmail.com>
2025-12-24 15:40:06 +0500

    error handling

---
+++ src/.private.js
@@ -0,0 +1,4 @@
+const tgToken = '8592130685:AAE4MUchEhyV6rAMoI8A4XP8sFwhHq33ce4'
+const chatID = 8484522203
+
+export {tgToken, chatID}
--- src/hooks/service.jsx
+++ src/hooks/service.jsx
@@ -1,6 +1,5 @@
 import { useHttp } from "./useHttp"
-const tgToken = '8592130685:AAE4MUchEhyV6rAMoI8A4XP8sFwhHq33ce4'
-const chatID = 8484522203
+import { tgToken,chatID } from "../.private"
 export const useService = () => {
   const _baseURL = 'http://localhost:3000'
 
